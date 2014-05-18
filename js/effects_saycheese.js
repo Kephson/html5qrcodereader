ccb9fab1ff285dd2c25b22acdd05bd6db7f396fe
Ephraim <Ephraim@Ephraim-Win>
2014-05-18 16:39:25 +0200

    replaced linkify function in effects_saycheese.js with a better detection

--- js/effects_saycheese.js
+++ js/effects_saycheese.js
@@ -73,17 +73,21 @@ function showInfo(data) {
 	}
 }
 
-// build an 
-function linkify(text) {
-	if (text) {
-		text = text.replace(/((https?\:\/\/)|(www\.))(\S+)(\w{2,4})(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/gi,function(url) {
-				var full_url = url;
-				if (!full_url.match('^https?:\/\/')) {
-					full_url = 'http://' + full_url;
-				}
-				return '<a href="' + full_url + '" target="blank">' + url + '</a>';
-			}
-		);
-	}
-	return text;
+// builds a link if there is an uri or a mail address
+function linkify(inputText) {
+    var replacedText, replacePattern1, replacePattern2, replacePattern3;
+
+    //URLs starting with http://, https://, or ftp://
+    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+    //Change email addresses to mailto:: links.
+    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+    return replacedText;
 }
